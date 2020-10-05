fc3c83908ebff3296c34129163975db1774d2ce7
Javieralonso Uceda Maza <javieralonso519@gmail.com>
2020-10-04 21:58:14 -0500

    Update Device.java

--- src/android/Device.java
+++ src/android/Device.java
@@ -181,8 +181,22 @@
     }
 
     public boolean isVirtual() {
-       return android.os.Build.FINGERPRINT.contains("generic") ||
-       android.os.Build.PRODUCT.contains("sdk");
+       return (android.os.Build.BRAND.startsWith("generic") && android.os.Build.DEVICE.startsWith("generic"))
+        || android.os.Build.FINGERPRINT.startsWith("generic")
+        || android.os.Build.FINGERPRINT.startsWith("unknown")
+        || android.os.Build.HARDWARE.contains("goldfish")
+        || android.os.Build.HARDWARE.contains("ranchu")
+        || android.os.Build.MODEL.contains("google_sdk")
+        || android.os.Build.MODEL.contains("Emulator")
+        || android.os.Build.MODEL.contains("Android SDK built for x86")
+        || android.os.Build.MANUFACTURER.contains("Genymotion")
+        || android.os.Build.PRODUCT.contains("sdk_google")
+        || android.os.Build.PRODUCT.contains("google_sdk")
+        || android.os.Build.PRODUCT.contains("sdk")
+        || android.os.Build.PRODUCT.contains("sdk_x86")
+        || android.os.Build.PRODUCT.contains("vbox86p")
+        || android.os.Build.PRODUCT.contains("emulator")
+        || android.os.Build.PRODUCT.contains("simulator");
    }
 
     /**
