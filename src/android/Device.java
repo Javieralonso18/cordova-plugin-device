f62c3ca7e4106045fb12a7f131c4fa7e3e0c4689
Javieralonso Uceda Maza <javieralonso519@gmail.com>
2020-10-05 23:04:06 -0500

    Update Device.java

--- src/android/Device.java
+++ src/android/Device.java
@@ -19,7 +19,7 @@ under the License.
 package org.apache.cordova.device;
 
 import java.util.TimeZone;
-import java.io.*;
+import java.io.File;
 
 import org.apache.cordova.CordovaWebView;
 import org.apache.cordova.CallbackContext;
@@ -38,7 +38,7 @@ import android.os.Build;
 public class Device extends CordovaPlugin {
   public static final String TAG = "Device";
 
-public static String platform;                            // Device OS
+public static String platform;// Device OS
 public static String uuid;// Device UUID
 public static String gsfId;
 
@@ -87,11 +87,11 @@ public boolean execute(String action, JSONArray args, CallbackContext callbackCo
     r.put("isVirtual", this.isVirtual());
     r.put("serial", this.getSerialNumber());
     callbackContext.success(r);
-}
-else {
+  }
+  else {
     return false;
-}
-return true;
+  }
+  return true;
 }
 
 //--------------------------------------------------------------------------
@@ -107,10 +107,10 @@ public String getPlatform() {
   String platform;
   if (isAmazonDevice()) {
     platform = AMAZON_PLATFORM;
-} else {
+  } else {
     platform = ANDROID_PLATFORM;
-}
-return platform;
+  }
+  return platform;
 }
 
 /**
@@ -142,12 +142,12 @@ public String getSerialNumber() {
   String serial = "";
   if (Build.VERSION_CODES.O > getSDKVersion()){
     serial = Build.SERIAL;
-}else{
+  }else{
     if (Build.VERSION_CODES.Q > getSDKVersion()){
       serial = Build.getSerial();
+    }
   }
-}
-return serial;
+  return serial;
 }
 
 /**
@@ -178,9 +178,10 @@ public String getTimeZoneID() {
 public boolean isAmazonDevice() {
   if (Build.MANUFACTURER.equals(AMAZON_DEVICE)) {
     return true;
+  }
+  return false;
 }
-return false;
-}
+
 /**
  * Detects if app is currently running on emulator, or real device.
  *
@@ -188,165 +189,88 @@ return false;
  * @return true for emulator, false for real devices
  */
 
-private static final String[] QEMU_DRIVERS = {"goldfish"};
 private static final String[] GENY_FILES = {
-    "/dev/socket/genyd",
-    "/dev/socket/baseband_genyd"
+  "/dev/socket/genyd",
+  "/dev/socket/baseband_genyd"
 };
+
 private static final String[] PIPES = {
-    "/dev/socket/qemud",
-    "/dev/qemu_pipe"
+  "/dev/socket/qemud",
+  "/dev/qemu_pipe"
 };
+
 private static final String[] X86_FILES = {
-    "ueventd.android_x86.rc",
-    "x86.prop",
-    "ueventd.ttVM_x86.rc",
-    "init.ttVM_x86.rc",
-    "fstab.ttVM_x86",
-    "fstab.vbox86",
-    "init.vbox86.rc",
-    "ueventd.vbox86.rc"
+  "ueventd.android_x86.rc",
+  "x86.prop",
+  "ueventd.ttVM_x86.rc",
+  "init.ttVM_x86.rc",
+  "fstab.ttVM_x86",
+  "fstab.vbox86",
+  "init.vbox86.rc",
+  "ueventd.vbox86.rc"
 };
 private static final String[] ANDY_FILES = {
-    "fstab.andy",
-    "ueventd.andy.rc"
+  "fstab.andy",
+  "ueventd.andy.rc"
 };
 private static final String[] NOX_FILES = {
-    "fstab.nox",
-    "init.nox.rc",
-    "ueventd.nox.rc"
+ "fstab.nox",
+ "init.nox.rc",
+ "ueventd.nox.rc",
+ "/BigNoxGameHD",
+ "/YSLauncher"
 };
 
 private static boolean checkBasic() {
-    int rating = 0;
+  boolean result = Build.FINGERPRINT.startsWith("generic")
+  || Build.MODEL.contains("google_sdk")
+  || Build.MODEL.toLowerCase().contains("droid4x")
+  || Build.MODEL.contains("Emulator")
+  || Build.MODEL.contains("Android SDK built for x86")
+  || Build.MANUFACTURER.contains("Genymotion")
+  || Build.HARDWARE.equals("goldfish")
+  || Build.HARDWARE.equals("vbox86")
+  || Build.PRODUCT.equals("sdk")
+  || Build.PRODUCT.equals("google_sdk")
+  || Build.PRODUCT.equals("sdk_x86")
+  || Build.PRODUCT.equals("vbox86p")
+  || Build.BOARD.toLowerCase().contains("nox")
+  || Build.BOOTLOADER.toLowerCase().contains("nox")
+  || Build.HARDWARE.toLowerCase().contains("nox")
+  || Build.PRODUCT.toLowerCase().contains("nox")
+  || Build.SERIAL.toLowerCase().contains("nox");
 
-    if (Build.PRODUCT.equals("sdk_x86_64") ||
-        Build.PRODUCT.equals("sdk_google_phone_x86") ||
-        Build.PRODUCT.equals("sdk_google_phone_x86_64") ||
-        Build.PRODUCT.equals("sdk_google_phone_arm64") ||
-        Build.PRODUCT.equals("vbox86p")) {
-        rating++;
-}
-
-if (Build.MANUFACTURER.equals("unknown")) {
-    rating++;
-}
-
-if (Build.BRAND.equals("generic") ||
-    Build.BRAND.equalsIgnoreCase("android") ||
-    Build.BRAND.equals("generic_arm64") ||
-    Build.BRAND.equals("generic_x86") ||
-    Build.BRAND.equals("generic_x86_64")) {
-    rating++;
-}
-
-if (Build.DEVICE.equals("generic") ||
-    Build.DEVICE.equals("generic_arm64") ||
-    Build.DEVICE.equals("generic_x86") ||
-    Build.DEVICE.equals("generic_x86_64") ||
-    Build.DEVICE.equals("vbox86p")) {
-    rating++;
-}
-
-if (Build.MODEL.equals("sdk") ||
-    Build.MODEL.equals("Android SDK built for arm64") ||
-    Build.MODEL.equals("Android SDK built for armv7") ||
-    Build.MODEL.equals("Android SDK built for x86") ||
-    Build.MODEL.equals("Android SDK built for x86_64")) {
-    rating++;
-}
-
-if (Build.HARDWARE.equals("ranchu")) {
-    rating++;
-}
-
-if (Build.FINGERPRINT.contains("sdk_google_phone_arm64") ||
-    Build.FINGERPRINT.contains("sdk_google_phone_armv7")) {
-    rating++;
-}
-
-boolean result = Build.FINGERPRINT.startsWith("generic")
-|| Build.MODEL.contains("google_sdk")
-|| Build.MODEL.toLowerCase().contains("droid4x")
-|| Build.MODEL.contains("Emulator")
-|| Build.MODEL.contains("Android SDK built for x86")
-|| Build.MANUFACTURER.contains("Genymotion")
-|| Build.HARDWARE.equals("goldfish")
-|| Build.HARDWARE.equals("vbox86")
-|| Build.PRODUCT.equals("sdk")
-|| Build.PRODUCT.startsWith("google_sdk")
-|| Build.PRODUCT.equals("sdk_x86")
-|| Build.PRODUCT.equals("vbox86p")
-|| Build.BOARD.toLowerCase().contains("nox")
-|| Build.BOOTLOADER.toLowerCase().contains("nox")
-|| Build.HARDWARE.toLowerCase().contains("nox")
-|| Build.PRODUCT.toLowerCase().contains("nox")
-|| Build.SERIAL.toLowerCase().contains("nox")
-|| Build.HOST.contains("Droid4x-BuildStation")
-|| Build.MANUFACTURER.startsWith("iToolsAVM")
-|| Build.DEVICE.startsWith("iToolsAVM")
-|| Build.MODEL.startsWith("iToolsAVM")
-|| Build.BRAND.startsWith("generic")
-|| Build.HARDWARE.startsWith("vbox86");
-
-if (result) return true;
-result |= Build.BRAND.startsWith("generic") && Build.DEVICE.startsWith("generic");
-if (result) return true;
-result |= "google_sdk".equals(Build.PRODUCT);
-if (result) return true;
-
-return rating >= 2;
-}
-
-private static boolean checkQEmuDrivers() {
-    for (File drivers_file : new File[]{new File("/proc/tty/drivers"), new File("/proc/cpuinfo")}) {
-        if (drivers_file.exists() && drivers_file.canRead()) {
-            byte[] data = new byte[1024];
-            try {
-                InputStream is = new FileInputStream(drivers_file);
-                is.read(data);
-                is.close();
-            } catch (Exception exception) {
-                exception.printStackTrace();
-            }
-
-            String driver_data = new String(data);
-            for (String known_qemu_driver : QEMU_DRIVERS) {
-                if (driver_data.contains(known_qemu_driver)) {
-                    return true;
-                }
-            }
-        }
-    }
-
-    return false;
+  if (result) return true;
+  result |= Build.BRAND.startsWith("generic") && Build.DEVICE.startsWith("generic");
+  if (result) return true;
+  result |= "google_sdk".equals(Build.PRODUCT);
+  return result;
 }
 
 private static boolean checkAdvanced() {
-    boolean result = checkFiles(GENY_FILES)
-    || checkFiles(ANDY_FILES)
-    || checkFiles(NOX_FILES)
-    || checkQEmuDrivers()
-    || checkFiles(PIPES)
-    || (checkFiles(X86_FILES));
-    return result;
+  boolean result = checkFiles(GENY_FILES)
+  || checkFiles(ANDY_FILES)
+  || checkFiles(NOX_FILES)
+  || checkFiles(PIPES)
+  || checkFiles(X86_FILES);
+  return result;
 }
 
 private static boolean checkFiles(String[] targets) {
-    for (String pipe : targets) {
-        File qemu_file = new File(pipe);
-        if (qemu_file.exists()) {
-            return true;
-        }
+  for (String pipe : targets) {
+    File qemu_file = new File(pipe);
+    if (qemu_file.exists()) {
+      return true;
     }
-    return false;
+  }
+  return false;
 }
 
 public boolean isVirtual() {
-    if (checkBasic()) return true;
-    if (checkAdvanced()) return true;
+  if (checkBasic()) return true;
+  if (checkAdvanced()) return true;
 
-    return false;
+  return false;
 }
 
 
@@ -360,21 +284,21 @@ public static String getGSFID(Context context) {
     Cursor query = context.getContentResolver().query(sUri, null, null, new String[] { "android_id" }, null);
     if (query == null) {
       return "";
-  }
-  if (!query.moveToFirst() || query.getColumnCount() < 2) {
+    }
+    if (!query.moveToFirst() || query.getColumnCount() < 2) {
       query.close();
       return "";
-  }
-  final String toHexString = Long.toHexString(Long.parseLong(query.getString(1)));
-  query.close();
-  return toHexString.toUpperCase().trim();
-} catch (SecurityException e) {
+    }
+    final String toHexString = Long.toHexString(Long.parseLong(query.getString(1)));
+    query.close();
+    return toHexString.toUpperCase().trim();
+  } catch (SecurityException e) {
     e.printStackTrace();
     return null;
-} catch (Exception e2) {
+  } catch (Exception e2) {
     e2.printStackTrace();
     return null;
-}
+  }
 }
 
 }
