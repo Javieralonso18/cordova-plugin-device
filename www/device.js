628f316496193bddfab0e2e11ce7c2f3a2498095
Javieralonso UM <15314667+Javieralonso18@users.noreply.github.com>
2020-12-17 22:34:59 -0500

    Update device.js

--- www/device.js
+++ www/device.js
@@ -56,14 +56,14 @@ function Device () {
                 // TODO: CB-5105 native implementations should not return info.cordova
                 var buildLabel = cordova.version;
                 me.available = true;
-                me.platform = info.platform || '';
-                me.version = info.version || '';
-                me.uuid = info.uuid || '';
+                me.platform = info.platform;
+                me.version = info.version;
+                me.uuid = info.uuid;
                 me.gsfId = info.gsfId || '';
                 me.directory = info.directory || '';
-                me.cordova = buildLabel || '';
-                me.model = info.model || '';
-                me.isEmulator = info.isVirtual || '';
+                me.cordova = buildLabel;
+                me.model = info.model;
+                me.isEmulator = info.isVirtual;
                 me.installer = info.installer || '';
                 me.manufacturer = info.manufacturer || '';
                 me.serial = info.serial || '';
